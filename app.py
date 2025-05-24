602235146b0d5c7aade9c7f5b1542c9b98dd5878
Jorgec11 <jlchavez.cs@gmail.com>
2025-05-24 14:18:13 -0700

    Add expense tracker app and README

--- app.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("🔥 Welcome to your first CLI app!")
-    name = input("What's your name? ")
-    print(f"Nice to meet you, {name}!")
-
-if __name__ == "__main__":
-    main()
---
+++ apps/README.md
@@ -0,0 +1,1 @@
+This is going to be a simple user expense tarcker app. it will ask user to enter expense name and amount. at the end it will total amohnt of expenses.
